382724a4ddbaac878086149994fd1fd25db7f0af
Zebby <48347420+TwilightZebby@users.noreply.github.com>
2021-01-10 21:38:04 +0000

    Add /boop Command

---
+++ slashCommands/boop.js
@@ -0,0 +1,41 @@
+// LIBRARY IMPORTS
+const fs = require('fs');
+const Discord = require("discord.js");
+
+// MODULE IMPORTS
+const ActionModule = require('../bot_modules/actionModule.js');
+
+// VARIABLE IMPORTS
+const { client } = require('../constants.js');
+const { PREFIX } = require('../config.js');
+
+
+
+// THIS COMMAND
+module.exports = {
+    name: 'hug',
+    description: 'Give someone a comforting cuddle',
+
+    // LIMITATIONS
+    //     'twilightzebby' - Only TwilightZebby#1955 can use this command
+    //     If commented out, everyone can use this command
+    //limitation: 'twilightzebby',
+
+    // Command's cooldown, in seconds
+    cooldown: 5,
+
+    /**
+     * Command's functionality
+     * 
+     * @param {Discord.Guild} guild 
+     * @param {*} data
+     * @param {*} commandData
+     * @param {Discord.GuildMember} member
+     */
+    async execute(guild, data, commandData, member) {
+
+      return await ActionModule.Respond("boop", guild, data, commandData, member);
+
+      // END OF SLASH COMMAND
+    }
+};
