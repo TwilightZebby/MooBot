e4e5cd5350f321a9cf9b5031130d215d70c4f3b9
Zebby <48347420+TwilightZebby@users.noreply.github.com>
2022-01-12 13:51:51 +0000

    Add `/muda` Action Command
    ...and with this, makes 10 permanent Slash Commands!

---
+++ slashCommands/muda.js
@@ -0,0 +1,73 @@
+// Imports
+const Discord = require('discord.js');
+//const fs = require('fs');
+const { client } = require('../constants.js');
+const CONSTANTS = require('../constants.js');
+const Action = require('../modules/actionModule.js');
+
+module.exports = {
+    // Slash Command's Name, MUST BE LOWERCASE AND NO SPACES
+    name: 'muda',
+    // Slash Command's description
+    description: `Pummel someone with a MUDA MUDA MUDA MUDA MUDAAAAAA!`,
+    // Category of Slash Command, used for Help (text) Command
+    category: 'action',
+
+    // Slash Command's Cooldown, in seconds
+    // If not provided or is commented out, will default to 3 seconds
+    cooldown: 5,
+
+
+    /**
+     * Returns data used for registering this Slash Command
+     * 
+     * @returns {Discord.ChatInputApplicationCommandData}
+     */
+    registerData()
+    {
+        const data = {};
+
+        // Slash Command's Name, Description, and Application Command Type
+        data.name = this.name;
+        data.description = this.description;
+        data.type = "CHAT_INPUT";
+        data.options = [
+            {
+                type: "MENTIONABLE",
+                name: "person",
+                description: "The person you want to pummel",
+                required: true
+            },
+            {
+                type: "BOOLEAN",
+                name: "gif",
+                description: "True to display a GIF, otherwise leave blank or use False",
+                required: false
+            }
+        ];
+        
+        return data;
+    },
+
+
+
+
+    /**
+     * Main function that runs this Slash Command
+     * 
+     * @param {Discord.CommandInteraction} slashCommand Slash Command Interaction
+     */
+    async execute(slashCommand)
+    {
+
+        // Ensure no DM usage
+        if ( slashCommand.channel instanceof Discord.DMChannel )
+        {
+            return await slashCommand.reply({ content: CONSTANTS.errorMessages.SLASH_COMMAND_GUILDS_ONLY, ephemeral: true });
+        }
+        else
+        {
+            return await Action.slashRespond(slashCommand);
+        }
+    }
+};
