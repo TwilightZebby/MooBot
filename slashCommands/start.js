9886b1611c0d0ac8feac23938ebf73e9c54ea5a4
Zebby <48347420+TwilightZebby@users.noreply.github.com>
2022-01-24 19:12:39 +0000

    So they did tweak IDs

--- slashCommands/start.js
+++ slashCommands/start.js
@@ -7,7 +7,7 @@ const CONSTANTS = require('../constants.js');
 
 // Activity IDs
 const ActivityIDs = new Discord.Collection().set("poker", "755827207812677713")
-.set("chess", "832012774040141894").set("sketch", "878067389634314250")
+.set("chess", "832012774040141894").set("sketch", "902271654783242291")
 .set("youtube", "880218394199220334").set("letter", "879863686565621790")
 .set("snacks", "879863976006127627").set("spell", "852509694341283871")
 .set("checkers", "832013003968348200");
