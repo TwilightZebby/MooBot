161c2ee4ed56d770540dd90da9f9e5261a656576
Zebby <48347420+TwilightZebby@users.noreply.github.com>
2022-02-16 14:52:38 +0000

    Tweak placeholders

--- selects/createrolemenu.js
+++ selects/createrolemenu.js
@@ -35,16 +35,16 @@ module.exports = {
                 let embedModal = new Discord.Modal().setCustomId("createembeddata").setTitle("Configure Menu Embed").addComponents([
                     new Discord.MessageActionRow().addComponents( new Discord.TextInputComponent().setCustomId("title").setLabel("Embed Title").setMaxLength(256).setStyle("SHORT").setRequired(true).setValue(!embedData?.title ? "" : embedData.title) ),
                     new Discord.MessageActionRow().addComponents( new Discord.TextInputComponent().setCustomId("description").setLabel("Embed Description").setMaxLength(4000).setStyle("PARAGRAPH").setValue(!embedData?.description ? "" : embedData.description) ),
-                    new Discord.MessageActionRow().addComponents( new Discord.TextInputComponent().setCustomId("hexcolour").setLabel("Embed Colour (In Hex Format, eg: #5865F2)").setMaxLength(7).setMinLength(7).setStyle("SHORT").setValue(!embedData?.hexColor ? "" : embedData.hexColor).setPlaceholder("#5865F2") )
+                    new Discord.MessageActionRow().addComponents( new Discord.TextInputComponent().setCustomId("hexcolour").setLabel("Embed Colour (In Hex Format)").setMaxLength(7).setMinLength(7).setStyle("SHORT").setValue(!embedData?.hexColor ? "" : embedData.hexColor).setPlaceholder("Example: #5865F2") )
                 ]);
                 await selectInteraction.showModal(embedModal);
                 break;
 
             case "add_role":
                 let newRoleModal = new Discord.Modal().setCustomId("createaddrole").setTitle("Add Assignable Role").addComponents([
-                    new Discord.MessageActionRow().addComponents( new Discord.TextInputComponent().setCustomId("roleid").setLabel("Role ID").setMaxLength(20).setMinLength(17).setStyle("SHORT").setRequired(true).setPlaceholder("598145033069264907") ),
+                    new Discord.MessageActionRow().addComponents( new Discord.TextInputComponent().setCustomId("roleid").setLabel("Role ID").setMaxLength(20).setMinLength(17).setStyle("SHORT").setRequired(true).setPlaceholder("Example: 598145033069264907") ),
                     new Discord.MessageActionRow().addComponents( new Discord.TextInputComponent().setCustomId("buttonlabel").setLabel("Role's Button Label (Required if no emoji)").setMaxLength(80).setStyle("SHORT") ),
-                    new Discord.MessageActionRow().addComponents( new Discord.TextInputComponent().setCustomId("buttonemoji").setLabel("Role's Button Emoji (Required if no label)").setStyle("SHORT").setPlaceholder("<:grass_block:601353406577246208>") )
+                    new Discord.MessageActionRow().addComponents( new Discord.TextInputComponent().setCustomId("buttonemoji").setLabel("Role's Button Emoji (Required if no label)").setStyle("SHORT").setPlaceholder("Example: <:grass_block:601353406577246208>") )
                 ]);
                 await selectInteraction.showModal(newRoleModal);
                 break;
