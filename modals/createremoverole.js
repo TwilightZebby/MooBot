55d43a7031b7fb97da0bd1079b839611297335bf
Zebby <48347420+TwilightZebby@users.noreply.github.com>
2022-02-24 14:59:48 +0000

    Add existing role check

--- modals/createremoverole.js
+++ modals/createremoverole.js
@@ -6,9 +6,9 @@ const CONSTANTS = require('../constants.js');
 
 module.exports = {
     // Modal's Name, used as start of its Custom ID
-    name: 'modal_name',
+    name: 'createremoverole',
     // Modal's description, purely for documentation
-    description: `Description`,
+    description: `Used to fetch the ID of the Role to be removed from the Menu`,
 
 
 
@@ -19,8 +19,28 @@ module.exports = {
      */
     async execute(modalInteraction)
     {
+        let inputRoleID = modalInteraction.fields.getTextInputValue("roleid");
 
-        // .
+        // Validate Role does indeed exist on the Menu
+        let roleCache = client.roleMenu.get("createMenuRoleCache");
+        if ( !roleCache )
+        {
+            await modalInteraction.update({ components: [CONSTANTS.components.selects.ROLE_MENU_CREATE] });
+            return await modalInteraction.followUp({ content: CONSTANTS.errorMessages.GENERIC, ephemeral: true });
+        }
 
+        let doesRoleExist = false;
+        for (const roleCacheObject of roleCache)
+        {
+            if ( roleCacheObject.roleID === inputRoleID ) { doesRoleExist = true; break; }
+        }
+
+        if ( !doesRoleExist )
+        {
+            await modalInteraction.update({ components: [CONSTANTS.components.selects.ROLE_MENU_CREATE] });
+            return await modalInteraction.followUp({ content: `That Role ID doesn't seem to match an existing Role on the Menu.\nPlease try again, ensuring the Role ID is for a Role already on this Menu`, ephemeral: true });
+        }
+
+        return await modalInteraction.followUp({ content: `Test go brrrrrr`, ephemeral: true });
     }
 };
