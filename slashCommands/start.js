fc75aa549eccca76bbe43780d6f2da3390ad1bc7
Zebby <48347420+TwilightZebby@users.noreply.github.com>
2022-03-10 19:49:12 +0000

    Rename `ocho` to `blazing 8s`

--- slashCommands/start.js
+++ slashCommands/start.js
@@ -10,7 +10,7 @@ const ActivityIDs = new Discord.Collection().set("poker", "755827207812677713")
 .set("chess", "832012774040141894").set("sketch", "902271654783242291")
 .set("youtube", "880218394199220334").set("letter", "879863686565621790")
 .set("snacks", "879863976006127627").set("spell", "852509694341283871")
-.set("checkers", "832013003968348200").set("ocho", "832025144389533716")
+.set("checkers", "832013003968348200").set("blazing", "832025144389533716")
 .set("putt", "945737671223947305");
 
 
@@ -62,7 +62,7 @@ module.exports = {
                     { name: "Word Snacks", value: "snacks" },
                     { name: "SpellCast", value: "spell" },
                     { name: "Checkers in the Park", value: "checkers" },
-                    { name: "Ocho", value: "ocho" },
+                    { name: "Blazing 8s", value: "blazing" },
                     { name: "Putt Party", value: "putt" }
                 ]
             }
