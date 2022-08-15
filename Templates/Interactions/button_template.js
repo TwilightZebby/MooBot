f42e2ae03f875103651aa4929624c85c4a832cb1
Zebby <48347420+TwilightZebby@users.noreply.github.com>
2022-08-15 10:28:13 +0100

    Move Interaction Template Files

--- Templates/Interactions/button_template.js
+++ Templates/Interactions/button_template.js
@@ -1,7 +1,7 @@
 const { ButtonInteraction } = require("discord.js");
-const { DiscordClient, Collections } = require("../constants.js");
-const LocalizedErrors = require("../JsonFiles/errorMessages.json");
-const LocalizedStrings = require("../JsonFiles/stringMessages.json");
+const { DiscordClient, Collections } = require("../../constants.js");
+const LocalizedErrors = require("../../JsonFiles/errorMessages.json");
+const LocalizedStrings = require("../../JsonFiles/stringMessages.json");
 
 module.exports = {
     // Button's Name
