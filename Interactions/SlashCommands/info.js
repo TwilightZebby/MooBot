737c5dc09ec6a439bfaedfbc6cb656387e6da9a5
Zebby <48347420+TwilightZebby@users.noreply.github.com>
2023-03-13 10:25:08 +0000

    `/info role` - Add support for Server Subs Roles

--- Interactions/SlashCommands/info.js
+++ Interactions/SlashCommands/info.js
@@ -1208,6 +1208,8 @@ ${ExternalEmojiPermission ? `${EMOJI_CHANNEL_FORUM} ` : ""}**Forum:** ${forumCha
                 });
             }
             if ( RoleOption.tags.premiumSubscriberRole != undefined ) { roleTagString += `${roleTagString.length > 4 ? `\n` : ""}**Is Server Booster Role:** ${RoleOption.tags.premiumSubscriberRole}`; }
+            if ( RoleOption.tags.subscriptionListingId != undefined ) { roleTagString += `${roleTagString.length > 4 ? `\n` : ""}**Is a Server Subscription Role:** true`; }
+            if ( RoleOption.tags.availableForPurchase != undefined ) { roleTagString += `${roleTagString.length > 4 ? `\n` : ""}**Is Purchasable:** true`; }
 
             if ( roleTagString.length > 4 ) { RoleInfoEmbed.addFields({ name: `>> Role Tags`, value: roleTagString }); }
         }
